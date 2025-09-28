e7a7957565aa2b38c0da1e4aa1bc08daac5aeec9
habib rouatbi <habi.rouatbi123@gmail.com>
2025-09-28 20:26:35 +0200

    feat : added fonts and style

---
+++ lib/presentation/theme/styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+TextStyle robotoRegular = const TextStyle(
+  color: AppColors.primaryTxt,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle robotoMedium = const TextStyle(
+  color: AppColors.primaryTxt,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w500,
+);
+
+
+TextStyle robotoBold = const TextStyle(
+  color: AppColors.primaryTxt,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle robotoSemiBold = const TextStyle(
+  color: AppColors.primaryTxt,
+  fontFamily: "Satoshi",
+  fontWeight: FontWeight.w600,
+);
