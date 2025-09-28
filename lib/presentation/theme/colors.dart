797d0f597c853cbc5534623c8ede30d10ff9f387
habib rouatbi <habi.rouatbi123@gmail.com>
2025-09-28 20:24:24 +0200

    feat : added colors

---
+++ lib/presentation/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff3085FE); // primary 5000
+  static const accent = Color(0xffD1E2F9); // primary 100
+  static const primary300 = Color(0xff7FA7DF);
+  static const primary400= Color(0xff588FDE); // primary 300
+  static const secondary = Color(0xffA3D139);
+  static const secondary300 = Color(0xffBCDE6B);
+  static const scaffold = Color(0xffFFFFFF);
+  static const primaryTxt = Color(0xff282B2E);
+  static const  secondaryTxt = Color(0xff404245);
+  static const primaryGrey = Color(0xffACAFB5);
+  static const  secondaryGrey = Color(0xffBDBFC4);
+  static const orangeAccent= Color(0xffffe9e1);
+}
