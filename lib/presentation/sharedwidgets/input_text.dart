5e6bc1f7ff3d25719c56d2332dc91dffd10fa324
habib rouatbi <habi.rouatbi123@gmail.com>
2025-09-28 21:49:23 +0200

    feat : added customm textFiled

---
+++ lib/presentation/sharedwidgets/input_text.dart
@@ -0,0 +1,89 @@
+
+import 'package:flutter/material.dart';
+import 'package:projectflow_web/presentation/theme/colors.dart';
+import 'package:projectflow_web/presentation/theme/styles.dart';
+
+class InputText extends StatelessWidget {
+  InputText(
+      {super.key,
+        this.readOnly = false,
+        this.controller,
+        this.focusNode ,
+        this.textInputType = TextInputType.text,
+        this.hintText = "",
+        this.borderRadius = 7,
+        this.validator,
+        this.onChanged,
+        this.suffixIcon,
+        this.prefixIcon,
+        this.fillColor  ,
+        this.obscureText = false,
+        this.maxLines = 1,
+        this.onTap,
+        this.filled = true ,
+        this.isBorder =true ,
+        this.onSaved
+      });
+  final bool readOnly;
+  final bool obscureText;
+  final TextEditingController? controller;
+  final FocusNode? focusNode ;
+  final TextInputType textInputType;
+  final String hintText;
+  double borderRadius;
+  final String? Function(String? s)? validator;
+  void Function(String)? onChanged ;
+  void Function()? onTap;
+  final Function(String? s)? onSaved;
+  Widget? suffixIcon;
+  Widget? prefixIcon ;
+  int maxLines;
+  //BorderSide borderSide ;
+  bool filled ;
+  bool isBorder;
+  Color? fillColor ;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLines: maxLines,
+      readOnly: readOnly,
+      focusNode: focusNode,
+      // cursorColor: Colors.black,
+      controller: controller,
+      keyboardType: textInputType,
+      style: sataoshiRegular.copyWith(fontSize: 13) ,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(18),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
+        filled: true,
+        fillColor: fillColor ?? Theme.of(context).inputDecorationTheme.fillColor,
+        hintText: hintText,
+        hintStyle: Theme.of(context).inputDecorationTheme.hintStyle ,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(
+              color: AppColors.primaryGrey,
+              style: isBorder
+                  ? BorderStyle.solid
+                  : BorderStyle.none
+          ),
+        ) ,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+          borderSide: BorderSide(
+              color: AppColors.primary400,
+              style: isBorder
+                  ? BorderStyle.solid
+                  : BorderStyle.none
+          ),
+        ),
+      ),
+      validator: validator,
+      onChanged: onChanged,
+      onTap: onTap,
+      obscureText: obscureText,
+
+    );
+  }
+}
