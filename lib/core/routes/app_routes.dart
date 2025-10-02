336001c8080dd1450cbabe4ef60c39ae70da95fe
habib rouatbi <habi.rouatbi123@gmail.com>
2025-10-02 21:41:07 +0200

    fix : fix routes issue

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,13 +1,29 @@
 
-import 'package:auto_route/auto_route.dart';
-import 'package:projectflow_web/core/routes/app_routes.gr.dart';
 
-@AutoRouterConfig()
-class AppRouter extends RootStackRouter {
-  @override
-  List<AutoRoute> get routes => [
-    AutoRoute(page: LoginRoute.page ,initial: true) ,
+import 'package:go_router/go_router.dart';
+import 'package:projectflow_web/presentation/features/auth/views/screens/login_screen.dart';
+import 'package:projectflow_web/presentation/features/auth/views/screens/register_screen.dart';
 
+final GoRouter router = GoRouter(
+  initialLocation: '/login',
+  debugLogDiagnostics: true,
+  routes: [
 
-  ];
-}
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginScreen(),
+      /*  redirect: (context, state) {
+         final station = LocalStorage.getStationFromLocal();
+          if (station != null) {
+            return '/dashboard';
+          }
+          return null;
+        }*/
+    ),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+
+  ],
+);
